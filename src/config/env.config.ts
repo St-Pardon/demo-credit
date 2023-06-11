c0741b76f5c88b0bb70ae7c8e162cbd7d8cc95f3
St Pardon <okc4pardon@gmail.com>
2023-06-11 19:36:21 +0100

    :new: created file to handle .env variables

---
+++ src/config/env.config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT: string = process.env.PORT || '5000';
+
+// database config
+export const CLIENT: string = process.env.CLIENT || 'mysql2';
+export const USER: string = process.env.DB_USER || 'root';
+export const PASSWORD: string = process.env.PASSWORD || 'St_pardon1';
+export const DATABASE: string = process.env.DATABASE || 'dcredit';
+export const NODE_ENV: string = process.env.NODE_ENV || 'development';
+
+// Auth Config
+export const JWT_SECRET: string = process.env.JWT_SECRET || '';
